459db5ce6c2017ee7821daa48fcd5a0ee7c59045
小林亮介 <kobayashiryousuke@kobayashiryousukenoMacBook-Pro.local>
2020-03-17 00:12:50 +0900

    [fix] AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,44 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        /*
+        |
+        --------------------------------------------------------------------------
+        | Component
+        |
+        --------------------------------------------------------------------------
+        */
+        $this->app->bind('arrayUtil', function(){
+            return app('App\Components\ArrayUtil');
+        });
+
+        $this->app->bind('apiResponseBuilder', function(){
+            return app('App\Components\ResponseBuilder\ApiResponseBuilder');
+        });
+
+
+        /*
+        |
+        --------------------------------------------------------------------------
+        | Model
+        |
+        --------------------------------------------------------------------------
+        */
+        $this->app->bind('userModel', function(){
+            return app('App\Models\User');
+        });
+
+
+        /*
+        |
+        --------------------------------------------------------------------------
+        | Service
+        |
+        --------------------------------------------------------------------------
+        */
+        $this->app->bind('userService', function(){
+            return app('App\Services\User');
+        });
     }
 
     /**
