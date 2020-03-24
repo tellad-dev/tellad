d02034952fa91932349c29ca2d0fcb28e5da3abe
小林亮介 <kobayashiryousuke@kobayashiryousukenoMacBook-Pro.local>
2020-03-24 10:30:51 +0900

    [add] SelectUsersTypeForm

---
+++ resources/nuxt/components/templates/SelectUsersTemplate/SelectUsersTemplate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="select-users grey lighten-4">
+    <select-users-type-form @submit="$listeners['submit']" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    SelectUsersTypeForm: () => import('~/components/organisms/forms/SelectUsersTypeForm'),
+  },
+}
+</script>
+
+<style scoped>
+.select-users {
+  height: 100%;
+  padding: 68px 0;
+}
+</style>
---
+++ resources/nuxt/pages/signup/select-users.vue
@@ -0,0 +1,38 @@
+<template>
+  <select-users-template @submit="updateUsers" />
+</template>
+
+<script>
+export default {
+  components: {
+    SelectUsersTemplate: () => import('~/components/templates/SelectUsersTemplate'),
+  },
+  methods: {
+    async updateUsers(values) {
+      // await authStore
+      // .signUp({ email: values.email, password: values.password })
+      //   .then(() => {
+      //     this.$router.push({ name: 'signup/select-user' })
+      //   })
+      //   .catch(error => {
+      //     // エラー処理
+      //     console.log(error)
+      //   })
+      //   .finally(() => {
+      //     //
+      //   })
+      await console.log(values)
+
+      if (values.type === 'Guest') {
+        this.$router.push({ name: 'mypage-profile', params: { id: '' } })
+      }
+
+      if (values.type === 'Host') {
+        this.$router.push({ name: 'mypage' })
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
