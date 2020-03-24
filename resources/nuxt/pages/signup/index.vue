1ad252d70a2a38e9f33fe6fcb23de8307f1e22b5
小林亮介 <kobayashiryousuke@kobayashiryousukenoMacBook-Pro.local>
2020-03-24 09:30:02 +0900

    [fix] signup画面

--- resources/nuxt/pages/signup/index.vue
+++ resources/nuxt/pages/signup/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <signup-template />
+  <signup-template @submit="submitSignup" />
 </template>
 
 <script>
 export default {
   components: {
     SignupTemplate: () => import('~/components/templates/SignupTemplate'),
+  },
+
+  methods: {
+    async submitSignup(values) {
+      // await authStore
+      // .signUp({ email: values.email, password: values.password })
+      //   .then(() => {
+      //     this.$router.push({ name: 'signup/select-user' })
+      //   })
+      //   .catch(error => {
+      //     // エラー処理
+      //     console.log(error)
+      //   })
+      //   .finally(() => {
+      //     //
+      //   })
+      await console.log('formValues', values)
+      // this.$router.push({ name: 'signup/select-user' })
+    },
   },
 }
 </script>
