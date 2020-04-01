fc304b3bdbc59382d7b306c1947e43ed9260ea34
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2020-04-01 12:40:40 +0900

    [fix]ルーティング

--- routes/api.php
+++ routes/api.php
@@ -12,22 +12,18 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(["middleware" => "guest:api"], function () {
     Route::post("/login", "AuthController@login");
     Route::post('/register', 'AuthController@register');
-});
-
-Route::group(["middleware" => "auth:api"], function () {
-    //Auth API
+    // Auth API
     Route::get("/me", "AuthController@me");
     Route::post('/logout', 'AuthController@logout');
     Route::post('/refresh', 'AuthController@refresh');
     //REST API
-    Route::resource('users', 'Api\UserController');
-    Route::resource('adrequests', 'Api\UserController');
-    Route::resource('profiles', 'Api\ProfileController');
-    Route::resource('businesses', 'Api\BusinessController');
-    Route::resource('ads', 'Api\AdController');
-    Route::resource('shops', 'Api\ShopController');
-    Route::resource('spaces', 'Api\SpaceController');
-});
+    Route::resource('users','Api\UserController');
+    Route::resource('users/{user}/profiles','Api\ProfileController');
+    Route::resource('adrequests','Api\AdRequestController');
+    Route::resource('users/{user}/businesses','Api\BusinessController');
+    Route::resource('ads','Api\AdController');
+    Route::resource('users/{user}/shops','Api\ShopController');
+    Route::resource('spaces','Api\SpaceController');
+    Route::resource('spaceforms','Api\SpaceFormController');
